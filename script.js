3403fdf794f73e3e1433ae0accf717b93a4a0a0b
MrKUMASANDX <kuramitsutakumi0801@gmail.com>
2026-05-15 10:20:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,33 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
+// 要素
+const speedText = document.getElementById("speed");
+const distanceText = document.getElementById("distance");
+const followBtn = document.getElementById("followBtn");
+
+// 現在地追従モード
+let followMode = true;
+
+// ボタン
+followBtn.addEventListener("click", () => {
+
+    followMode = !followMode;
+
+    if (followMode) {
+
+        followBtn.textContent = "追従: ON";
+
+    } else {
+
+        followBtn.textContent = "追従: OFF";
+    }
+});
+
+// マーカー
 let marker;
 
-// 移動経路を保存
+// 経路
 let path = [];
 
 // 線
@@ -16,7 +40,14 @@ let polyline = L.polyline(path, {
     weight: 5
 }).addTo(map);
 
-// 現在地追跡
+// 距離
+let totalDistance = 0;
+
+// 前回位置
+let previousLat = null;
+let previousLng = null;
+
+// GPS追跡
 navigator.geolocation.watchPosition(
     success,
     error,
@@ -27,24 +58,65 @@ navigator.geolocation.watchPosition(
     }
 );
 
-// 成功時
+// 成功
 function success(position) {
 
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
 
-    console.log(lat, lng);
+    // 速度
+    let speed = position.coords.speed;
 
-    // 地図移動
-    map.setView([lat, lng], 17);
+    // null対策
+    if (speed === null) {
+        speed = 0;
+    }
 
-    // 配列へ追加
+    // m/s → km/h
+    speed = (speed * 3.6).toFixed(1);
+
+    speedText.textContent =
+        `速度: ${speed} km/h`;
+
+    // 地図追従
+    if (followMode) {
+        map.setView([lat, lng], 17);
+    }
+
+    // 線追加
     path.push([lat, lng]);
 
-    // 線更新
     polyline.setLatLngs(path);
 
-    // 最初だけ作成
+    // 距離計算
+    if (previousLat !== null) {
+
+        const distance = getDistance(
+            previousLat,
+            previousLng,
+            lat,
+            lng
+        );
+
+        totalDistance += distance;
+    }
+
+    previousLat = lat;
+    previousLng = lng;
+
+    // 表示
+    if (totalDistance < 1000) {
+
+        distanceText.textContent =
+            `移動距離: ${totalDistance.toFixed(1)} m`;
+
+    } else {
+
+        distanceText.textContent =
+            `移動距離: ${(totalDistance / 1000).toFixed(2)} km`;
+    }
+
+    // マーカー
     if (!marker) {
 
         marker = L.marker([lat, lng])
@@ -54,7 +126,6 @@ function success(position) {
 
     } else {
 
-        // マーカー移動
         marker.setLatLng([lat, lng]);
     }
 }
@@ -64,22 +135,35 @@ function error(err) {
 
     console.log(err);
 
-    switch(err.code) {
+    alert("位置情報を取得できません");
+}
 
-        case 1:
-            alert("位置情報が許可されていません");
-            break;
+// 距離計算
+function getDistance(lat1, lng1, lat2, lng2) {
 
-        case 2:
-            alert("位置情報を取得できません");
-            break;
+    const R = 6371000;
 
-        case 3:
-            alert("タイムアウトしました");
-            break;
+    const dLat =
+        (lat2 - lat1) * Math.PI / 180;
 
-        default:
-            alert("不明なエラー");
-            break;
-    }
+    const dLng =
+        (lng2 - lng1) * Math.PI / 180;
+
+    const a =
+        Math.sin(dLat / 2) *
+        Math.sin(dLat / 2) +
+
+        Math.cos(lat1 * Math.PI / 180) *
+        Math.cos(lat2 * Math.PI / 180) *
+
+        Math.sin(dLng / 2) *
+        Math.sin(dLng / 2);
+
+    const c =
+        2 * Math.atan2(
+            Math.sqrt(a),
+            Math.sqrt(1 - a)
+        );
+
+    return R * c;
 }
